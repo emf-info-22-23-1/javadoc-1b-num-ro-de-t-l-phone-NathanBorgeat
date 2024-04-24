93a4f5df9d6d2f1da6d85ca05057bebb136743b8
borgeatn <borgeatn@WSTEMFA44-20.stu.net.fr.ch>
2024-04-24 08:36:17 +0200

    Exercice terminé

--- src/ch/emf/atelierpo/wrk/Worker.java
+++ src/ch/emf/atelierpo/wrk/Worker.java
@@ -1,13 +1,22 @@
 package ch.emf.atelierpo.wrk;
 
-
 /**
  *
  * @author Mario Ramalho(EMF - Fribourg)
  */
 public class Worker {
 
-
+    /**
+     * Contrôle la validité d'un numéro de téléphone. Le préfixe doit être sous
+     * la forme suivante : "+41xy", x et y étant des chiffres entre 0 et 9. Le
+     * numéro de téléphone peut contenir uniquement les chiffres entre 0 et 9 et
+     * les espaces. Le retour est vrai si les conditions sont respectées, sinon
+     * il est faux.
+     *
+     * @param prefix le préfixe du numéro de téléphone
+     * @param numero le numero de téléphone sans le préfixe
+     * @return un booléen
+     */
     public boolean controleNumeroTelMobile(String prefix, String numero) {
         return false;
     }
